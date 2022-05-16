58c4a03f371d583b16c3fa4913e406a9695031a0
Joan Zorovich <joan.zorovich@jobandtalent.com>
2022-05-16 12:35:07 -0500

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,8 +1,12 @@
 module SessionsHelper
     
     # Logs in the given user.
+    
     def log_in(user)
         session[:user_id] = user.id
+        # Guard against session replay attacks.
+        # See https://bit.ly/33UvK0w for more.
+        session[:session_token] = user.session_token
     end
 
     # Remembers a user in a persistent session.
@@ -12,10 +16,15 @@ module SessionsHelper
         cookies.permanent[:remember_token] = user.remember_token
     end
 
-    # Returns the current logged-in user (if any).
+
+    # Returns the user corresponding to the remember token cookie.
+
     def current_user
         if (user_id = session[:user_id])
-            @current_user ||= User.find_by(id: user_id)
+            user = User.find_by(id: user_id)
+            if user && session[:session_token] == user.session_token
+                @current_user = user
+            end
         elsif (user_id = cookies.encrypted[:user_id])
             user = User.find_by(id: user_id)
             if user && user.authenticated?(cookies[:remember_token])
@@ -25,6 +34,12 @@ module SessionsHelper
         end
     end
 
+    # Returns true if the given user is the current user.
+    def current_user?(user)
+        user && user == current_user
+    end
+    
+
     # Returns true if the user is logged in, false otherwise.
     def logged_in?
         !current_user.nil?
@@ -32,7 +47,6 @@ module SessionsHelper
 
     # Logs out the current user.
     def log_out
-        forget(current_user)
         reset_session
         @current_user = nil
     end
@@ -44,4 +58,8 @@ module SessionsHelper
         cookies.delete(:remember_token)
     end
 
+    # Stores the URL trying to be accessed.
+    def store_location
+        session[:forwarding_url] = request.original_url if request.get?
+    end
 end
